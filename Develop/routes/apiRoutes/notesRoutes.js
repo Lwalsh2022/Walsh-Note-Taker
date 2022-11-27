72016df6ef432a12f16f5ab02870023e0d8f4f80
Lisa Walsh <lwalshphotography@yahoo.com>
2022-11-27 14:05:41 -0500

    modified server.js file

---
+++ Develop/routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { createNewNote, deleteNote } = require('../../lib/notes');
+let { notesArray } = require('../../db/notes');
+
+// notes are available at /api/notes in JSON format
+router.get('/notes', (req, res) => {
+    let results = notesArray;
+    res.json(results);
+    });
+
+    router.post('/notes', (req, res) => {
+        // set id based on what the next index of the array will be
+        if(notesArray) {
+            req.body.id = notesArray.length.toString();
+            } else {
+                req.body.id = '0'}
+                res.json(createNewNote(req.body, notesArray));  
+            });
+                
+module.exports = router;
