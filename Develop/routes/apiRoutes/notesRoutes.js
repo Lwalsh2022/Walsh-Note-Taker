a2215fb7f46d15be32e7e66b9f12735963571ba4
Lisa Walsh <lwalshphotography@yahoo.com>
2022-12-01 15:58:13 -0500

    still not working

--- Develop/routes/apiRoutes/notesRoutes.js
+++ Develop/routes/apiRoutes/notesRoutes.js
@@ -1,14 +1,16 @@
+// is this supposed to be cost notesRoutes = require('express').Router(); ?
+
 const router = require('express').Router();
 const { createNewNote, deleteNote } = require('../../lib/notes');
 let { notesArray } = require('../../db/notes');
 
 // notes are available at /api/notes in JSON format
-router.get('/notes', (req, res) => {
+app.get('/notes', (req, res) => {
     let results = notesArray;
     res.json(results);
     });
 
-    router.post('/notes', (req, res) => {
+    app.post('/notes', (req, res) => {
         // set id based on what the next index of the array will be
         if(notesArray) {
             req.body.id = notesArray.length.toString();
